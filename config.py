ab9aa4befd6f088e166cb47c217740053205faaa
Yakov Borevich <j.borevich@gmail.com>
2017-08-11 16:47:13 +0300

    Fit with 2.1.4 upcoming release

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def can_build(platform):
 
 def configure(env):
 	if env['platform'] == 'android':
-		env.android_add_dependency("compile 'com.android.support:appcompat-v7:23.0.1'")
-		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.22.1'")
+		#env.android_add_dependency("compile 'com.android.support:appcompat-v7:23.0.1'")
+		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4+'")
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("AndroidManifestChunk.xml")
