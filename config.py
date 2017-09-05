6a29fbea27d0a20bbb9df6e934628dd1d9d622f6
Yakov Borevich <j.borevich@gmail.com>
2017-09-05 10:50:03 +0300

    Inject fb version

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ def can_build(platform):
 def configure(env):
 	if env['platform'] == 'android':
 		#env.android_add_dependency("compile 'com.android.support:appcompat-v7:23.0.1'")
-		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4+'")
+		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.18.0'")
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("AndroidManifestChunk.xml")
